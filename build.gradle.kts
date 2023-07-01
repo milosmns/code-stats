6e37fd954a5c19fe53ec7c678dfb9ac9288827a7
Milos Marinkovic <milosmns@gmail.com>
2023-07-02 01:59:13 +0200

    Enable GitHub publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,10 @@
+import com.github.breadmoirai.githubreleaseplugin.ChangeLogSupplier
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
+import org.gradle.internal.impldep.org.joda.time.LocalDateTime
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinTargetContainerWithPresetFunctions
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinJvmCompilation
@@ -12,6 +18,7 @@ plugins {
   id("com.apollographql.apollo3") version "4.+"
   id("com.github.johnrengelman.shadow") version "8.+"
   id("org.jlleitschuh.gradle.ktlint") version "11.+"
+  id("com.github.breadmoirai.github-release") version "2.+"
 }
 
 repositories {
@@ -22,8 +29,8 @@ application {
   mainClass.set("MainKt")
 }
 
-group = Output.group
-version = Output.version
+group = Out.group
+version = Out.version
 
 kotlin {
   val jvmTarget = Configurator.configureJvmTarget(this)
@@ -89,71 +96,116 @@ kotlin {
 
   }
 
-  tasks.withType<Test> {
-    useJUnitPlatform()
-  }
+  tasks {
 
-  // make the JVM 'run' task work
-  tasks.withType<JavaExec> {
-    val jvmMainCompilation = jvmTarget.compilations.getByName<KotlinJvmCompilation>("main")
-    classpath(
-      files(
-        jvmMainCompilation.runtimeDependencyFiles,
-        jvmMainCompilation.output.allOutputs,
-      )
-    )
-  }
-
-  // make the JVM 'jar' task work
-  tasks.withType<ShadowJar> {
-    archiveBaseName.set(Output.artifact)
-    archiveClassifier.set("")
-    archiveVersion.set("")
-
-    val jvmMainCompilation = jvmTarget.compilations.getByName<KotlinJvmCompilation>("main")
-    from(jvmMainCompilation.output)
-
-    configurations = mutableListOf(
-      jvmMainCompilation.compileDependencyFiles,
-      jvmMainCompilation.runtimeDependencyFiles,
-    )
-  }
-
-  tasks.register<Copy>("install") {
-    group = "application"
-    description = "Build the native executable and install it"
-    val installDir = Output.getInstallDir()
-    val artifactName = Output.artifact
-
-    if (nativeTarget == null) {
-      // JVM: copy the file to the root directory
-      dependsOn("shadowJar")
-      val jarDir = Output.getJarDir()
-      from(jarDir) {
-        include("$artifactName.jar")
-      }
-      into(installDir)
-
-      doLast {
-        println("Copied $jarDir/$artifactName.jar to $installDir/$artifactName.jar")
-      }
-    } else {
-      // Native: copy the file to the programs directory
-      dependsOn("${nativeTarget.name}Binaries")
-      val binaryDir = Output.getBinaryDir(nativeTarget.name)
-      from(binaryDir) {
-        include("$artifactName.kexe")
-        rename { artifactName }
-      }
-      into(installDir)
-
-      doLast {
-        println("Copied $binaryDir/$artifactName.kexe to $installDir/$artifactName")
+    withType<Test> {
+      useJUnitPlatform()
+      testLogging {
+        exceptionFormat = FULL
+        events = setOf(PASSED, FAILED, SKIPPED)
       }
     }
-  }
 
-}
+    // make the JVM 'run' task work
+    withType<JavaExec> {
+      val jvmMainCompilation = jvmTarget.compilations.getByName<KotlinJvmCompilation>("main")
+      classpath(
+        files(
+          jvmMainCompilation.runtimeDependencyFiles,
+          jvmMainCompilation.output.allOutputs,
+        )
+      )
+    }
+
+    // make the JVM 'jar' task work
+    withType<ShadowJar> {
+      archiveBaseName.set(Out.artifact)
+      archiveClassifier.set("")
+      archiveVersion.set("")
+
+      val jvmMainCompilation = jvmTarget.compilations.getByName<KotlinJvmCompilation>("main")
+      from(jvmMainCompilation.output)
+
+      configurations = mutableListOf(
+        jvmMainCompilation.compileDependencyFiles,
+        jvmMainCompilation.runtimeDependencyFiles,
+      )
+    }
+
+    register<Copy>("prepareForPublish") {
+      group = "distribution"
+      description = "Prepare the project outputs for publishing"
+      val distributionsDir = Out.getDistributionsDir()
+      val artifactName = Out.artifact
+
+      val dependentTasks = mutableListOf("shadowJar")
+      if (nativeTarget != null) {
+        dependentTasks.add("${nativeTarget.name}Binaries")
+      }
+      dependsOn(*dependentTasks.toTypedArray()) // must be called only once with varargs
+
+      // take native if available
+      if (nativeTarget != null) {
+        from(Out.getBinaryDir(nativeTarget.name)) {
+          include("$artifactName.kexe")
+          rename { if (it.endsWith(".kexe")) artifactName else it }
+        }
+      }
+      // take JAR anyway
+      from(Out.getJarDir()) {
+        include("$artifactName.jar")
+      }
+
+      into(distributionsDir)
+
+      doLast {
+        println("Copied $artifactName artifact[s] to $distributionsDir")
+      }
+    }
+
+    register<Copy>("install") {
+      group = "application"
+      description = "Build the native executable and install it"
+      val installDir = Out.getInstallDir()
+      val artifactName = Out.artifact
+
+      if (nativeTarget == null) {
+        // JVM: copy the file to the root directory
+        dependsOn("shadowJar")
+
+        val jarDir = Out.getJarDir()
+        from(jarDir) {
+          include("$artifactName.jar")
+        }
+        into(installDir)
+
+        doLast {
+          println("Copied $jarDir/$artifactName.jar to $installDir/$artifactName.jar")
+        }
+      } else {
+        // Native: copy the file to the programs directory
+        dependsOn("${nativeTarget.name}Binaries")
+
+        val binaryDir = Out.getBinaryDir(nativeTarget.name)
+        from(binaryDir) {
+          include("$artifactName.kexe")
+          rename { artifactName }
+        }
+        into(installDir)
+
+        doLast {
+          println("Copied $binaryDir/$artifactName.kexe to $installDir/$artifactName")
+        }
+      }
+    } // install task
+
+    named("githubRelease") {
+      dependsOn("prepareForPublish")
+    }
+
+  } // tasks
+
+} // kotlin
 
 ktlint {
   verbose.set(true)
@@ -175,6 +227,92 @@ apollo {
       connectToKotlinSourceSet("commonMain")
     }
   }
+}
+
+githubRelease {
+  val writeToken = OS.env("GITHUB_TOKEN", "<invalid>")
+  if (writeToken == "<invalid>") println("Set \$GITHUB_TOKEN to enable GitHub releases.")
+
+  val commitish = OS.env("GITHUB_SHA", default = "master")
+  val prNumber = OS.env("GITHUB_PR_NUMBER", default = "")
+  val buildMoment = LocalDateTime.now()
+  val nowTag = buildMoment.toString("yyyy-MM-dd-HH-mm-ss")
+  val quality = OS.env("BUILD_QUALITY", default = "Development")
+  var directoryPrefix = ""
+  var prNumberPrefix = ""
+  var timestampSuffix = ""
+  when (quality) {
+    "GA" -> Unit
+    "PR" -> {
+      directoryPrefix = "pr/"
+      prNumberPrefix = if (prNumber.isNotBlank()) "$prNumber/" else ""
+      timestampSuffix = "/$nowTag"
+    }
+
+    else -> {
+      directoryPrefix = "dev/"
+      prNumberPrefix = ""
+      timestampSuffix = "/$nowTag"
+    }
+  }
+  val tag = "${directoryPrefix}${prNumberPrefix}v${Out.version}$timestampSuffix"
+  val name = "[$quality] v${Out.version}"
+
+  token(writeToken)
+  owner(Out.repoOwner)
+  repo(Out.repoName)
+  tagName(tag)
+  releaseName(name)
+  targetCommitish(commitish)
+  prerelease(quality != "GA")
+
+  val maxFetched = 15
+  val maxReported = 7
+  val bullet = "\n* "
+  val changelogConfig = closureOf<ChangeLogSupplier> {
+    currentCommit("HEAD")
+    lastCommit("HEAD~$maxFetched")
+    options("--format=oneline", "--abbrev-commit", "--max-count=$maxFetched")
+  }
+  val ignoredMessagesRegex = setOf(
+    "(?i).*bump.*version.*",
+    "(?i).*increase.*version.*",
+    "(?i).*version.*bump.*",
+    "(?i).*version.*increase.*",
+    "(?i).*merge.*request.*",
+    "(?i).*request.*merge.*",
+  ).map(String::toRegex)
+
+  val changes = try {
+    changelog(changelogConfig)
+      .call()
+      .trim()
+      .split("\n")
+      .map { it.trim() }
+      .filterNot { ignoredMessagesRegex.any(it::matches) }
+      .take(maxReported)
+  } catch (t: Throwable) {
+    System.err.println("Failed to fetch Git history. Reason: ${t.message}")
+    emptyList()
+  }
+  val changesAsBullets = changes.joinToString(separator = bullet, prefix = bullet)
+
+  body(
+    when {
+      changes.isNotEmpty() -> "## Latest changes\n$changesAsBullets"
+      else -> "Explore commit history for latest changes."
+    }
+  )
+
+  val distributionsDir = Out.getDistributionsDir()
+  val jarFile = file("$distributionsDir/${Out.artifact}.jar")
+  val configFile = file("src/commonMain/resources/sample.config.yaml")
+  val unixFile = file("$distributionsDir/${Out.artifact}")
+  val toInclude = mutableListOf(jarFile, configFile)
+  if (unixFile.exists()) toInclude += unixFile
+  releaseAssets(*toInclude.toTypedArray())
+
+  println("Configured GitHub release '$tag' for publishing. Run 'githubRelease' to publish.")
 }
 
 // region Utils
@@ -202,11 +340,12 @@ object OS {
     ?: default
 }
 
-object Output {
+object Out {
   val group = OS.env("PROJECT_GROUP", default = "xyz.marinkovic.milos")
   val artifact = OS.env("PROJECT_ARTIFACT", default = "codestats")
   val version = OS.env("PROJECT_VERSION", default = "0.1.0")
-  val author = OS.env("PROJECT_AUTHOR", default = "milosmns")
+  val repoOwner = OS.env("REPO_OWNER", default = "milosmns")
+  val repoName = OS.env("REPO_NAME", default = "code-stats")
 
   fun getInstallDir() = when (OS.currentPlatform) {
     OS.Platform.MAC -> "/usr/local/bin"
@@ -217,6 +356,9 @@ object Output {
     .replace('/', File.separatorChar)
 
   fun getBinaryDir(targetName: String) = "build/bin/$targetName/${artifact}ReleaseExecutable"
+    .replace('/', File.separatorChar)
+
+  fun getDistributionsDir() = "build/distributions"
     .replace('/', File.separatorChar)
 }
 
@@ -230,7 +372,7 @@ object Configurator {
           jvmTarget.set(JvmTarget.JVM_1_8)
         }
       }
-      println("Configured Kotlin/Native target '$name'")
+      println("Configured Kotlin target '$name'")
     }
 
   fun configureNativeTarget(container: KotlinTargetContainerWithPresetFunctions) =
@@ -243,7 +385,7 @@ object Configurator {
       else -> null
     }?.also { target ->
       target.binaries {
-        executable(Output.artifact) {
+        executable(Out.artifact) {
           entryPoint = "main"
         }
       }
