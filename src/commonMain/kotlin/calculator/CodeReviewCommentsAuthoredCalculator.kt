57e58b2cc9c7c2ab4b86752c7296da270b39f0f1
Milos Marinkovic <milosmns@gmail.com>
2023-08-06 22:00:10 +0200

    Implement comments authored calculator

---
+++ src/commonMain/kotlin/calculator/CodeReviewCommentsAuthoredCalculator.kt
@@ -0,0 +1,46 @@
+package calculator
+
+import components.data.Repository
+import components.metrics.CodeReviewCommentsAuthored
+
+class CodeReviewCommentsAuthoredCalculator : GenericLongMetricCalculator<CodeReviewCommentsAuthored> {
+
+  override fun calculate(repositories: List<Repository>): CodeReviewCommentsAuthored {
+    val perUser = repositories
+      .flatMap { repository -> repository.codeReviews }
+      .flatMap { codeReview -> codeReview.comments }
+      .groupBy { comment -> comment.author }
+      .mapValues { (_, comments) -> comments.size.toLong() }
+
+    val perReviewer = repositories
+      .flatMap { repository -> repository.codeReviews }
+      .flatMap { codeReview -> codeReview.requestedReviewers }
+      .toSet()
+      .associateWith { reviewer ->
+        repositories
+          .flatMap { repository -> repository.codeReviews }
+          .filter { codeReview -> codeReview.requestedReviewers.contains(reviewer) }
+          .flatMap { codeReview -> codeReview.comments }
+          .count { comment -> comment.author == reviewer }
+          .toLong()
+      }
+
+    val perCodeReview = repositories
+      .flatMap { repository -> repository.codeReviews }
+      .associateWith { codeReview -> codeReview.comments.count().toLong() }
+
+    val perRepository = repositories
+      .associateWith { repository ->
+        repository.codeReviews
+          .sumOf { codeReview -> codeReview.comments.count().toLong() }
+      }
+
+    return CodeReviewCommentsAuthored(
+      perAuthor = perUser,
+      perReviewer = perReviewer,
+      perCodeReview = perCodeReview,
+      perRepository = perRepository
+    )
+  }
+
+}
