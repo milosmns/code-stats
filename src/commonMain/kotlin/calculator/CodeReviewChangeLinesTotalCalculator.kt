047438006a0f4d040c42799bd116b61ed722d098
Milos Marinkovic <milosmns@gmail.com>
2023-08-19 21:59:34 +0200

    Implement change lines total calculator

---
+++ src/commonMain/kotlin/calculator/CodeReviewChangeLinesTotalCalculator.kt
@@ -0,0 +1,45 @@
+package calculator
+
+import components.data.Repository
+import components.metrics.CodeReviewChangeLinesTotal
+
+class CodeReviewChangeLinesTotalCalculator : GenericLongMetricCalculator<CodeReviewChangeLinesTotal> {
+
+  override fun calculate(repositories: List<Repository>): CodeReviewChangeLinesTotal {
+    val perUser = repositories
+      .flatMap { repository -> repository.codeReviews }
+      .groupBy { codeReview -> codeReview.author }
+      .mapValues { (_, codeReviews) ->
+        codeReviews.sumOf { it.countLinesTotal() }
+      }
+
+    val perReviewer = repositories
+      .flatMap { repository -> repository.codeReviews }
+      .flatMap { codeReview -> codeReview.requestedReviewers }
+      .toSet()
+      .associateWith { reviewer ->
+        repositories
+          .flatMap { repository -> repository.codeReviews }
+          .filter { codeReview -> codeReview.requestedReviewers.contains(reviewer) }
+          .sumOf { codeReview -> codeReview.countLinesTotal() }
+      }
+
+    val perCodeReview = repositories
+      .flatMap { repository -> repository.codeReviews }
+      .associateWith { codeReview -> codeReview.countLinesTotal() }
+
+    val perRepository = repositories
+      .associateWith { repository ->
+        repository.codeReviews
+          .sumOf { codeReview -> codeReview.countLinesTotal() }
+      }
+
+    return CodeReviewChangeLinesTotal(
+      perAuthor = perUser,
+      perReviewer = perReviewer,
+      perCodeReview = perCodeReview,
+      perRepository = perRepository,
+    )
+  }
+
+}
